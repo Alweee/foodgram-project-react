0c314d8a31358b51396d4e7329c65f7147a2fcd5
Sasha <sasha95296@mail.ru>
2022-10-06 11:38:20 +0300

    override UserViewSet

--- backend/foodgram/users/urls.py
+++ backend/foodgram/users/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from users.views import CustomUserViewSet
+
+router_v1 = DefaultRouter()
+router_v1.register('users', CustomUserViewSet)
 
 urlpatterns = [
-    path('', include('djoser.urls')),
+    path('', include(router_v1.urls)),
     path('auth/', include('djoser.urls.authtoken')),
 ]
--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -1,8 +1,13 @@
 from djoser.views import UserViewSet
-from models import User
+from rest_framework import permissions
+from users.models import User
+from users.serializers import CustomUserSerializer
 
 
 class CustomUserViewSet(UserViewSet):
+    serializer_class = CustomUserSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
     def get_queryset(self):
         queryset = User.objects.all()
         return queryset
